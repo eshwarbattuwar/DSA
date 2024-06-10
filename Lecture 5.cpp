19169ef531992a0010b5694f836e56f2d622d0bc
Eshwar Battuwar <118370485+eshwarbattuwar@users.noreply.github.com>
2024-06-10 12:35:34 +0530

    Add files via upload

---
+++ Lecture 5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	itn c,d;
+	cout<<"Enter 4 integers";
+	cin>>a>>b>>c>>d;
+	//statement: a>b
+	cout<<(a>b && c>d);
+	//True as 1 false as 0
+}
