fdf8f2fe34ca0872ba52fbb6dd9a06e2da4f598c
Eshwar Battuwar <118370485+eshwarbattuwar@users.noreply.github.com>
2024-06-11 12:28:52 +0530

    Add files via upload

---
+++ Lecture 7.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c,d,e;
+	cout<<"Give 5 integers "<<endl;
+	cin>>a>>b>>c>>d>>e;
+	cout<<"the integers in reverse order are: "<<e<<d<<c<<b<<a;
+	
+}
