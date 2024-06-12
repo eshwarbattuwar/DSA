0fcad00b87141a2ba14ff4dfa64a872578e9d990
Eshwar Battuwar <118370485+eshwarbattuwar@users.noreply.github.com>
2024-06-12 20:00:49 +0530

    Add files via upload

---
+++ Lecture 12.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//int main(){
+//	int password;
+//	do{
+//		cin>>password;
+//	}
+//	while(password<999999);
+//
+//}
+
+//infinite loop
+int main(){
+	int password;
+	while(1)    //until the condition is true it prints the infilte loop
+	{
+		cout<<"chocolates"<<endl;
+	}
+}
+
