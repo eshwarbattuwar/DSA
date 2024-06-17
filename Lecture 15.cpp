699f118efaa8b27017d92cba79feee7a3f629a94
Eshwar Battuwar <118370485+eshwarbattuwar@users.noreply.github.com>
2024-06-17 12:17:17 +0530

    Add files via upload

---
+++ Lecture 15.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int compare(int a,int b)
+//{
+//	
+//	if(a>b)
+//	{
+//		return a;
+//	}
+//	else{
+//		return b;
+//	}
+//}
+
+{
+	if(a==5){
+		return 1;
+	}
+	return 0;
+}
+
+
+
