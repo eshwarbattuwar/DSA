304d9a62d456a7f92df5caa6a5056dc61609601c
Eshwar Battuwar <118370485+eshwarbattuwar@users.noreply.github.com>
2024-06-20 12:13:53 +0530

    Add files via upload

---
+++ Lecture 20.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	long long int a;
+	int b;
+	float c;
+	cout<<"the size of int is : "<<sizeof(b)<<" the size of long long int is: "<<sizeof(a);
+	cout<<"the address of a is : "<<&a<<" The address of b is : "<<&b;
+	cout<<"the address of c is : "<<&c;
+	return 0;
+}
